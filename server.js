333b31a05e775bcf5c2615c8f6acbfbeeed5a120
Seth Gerou <seth.gerou@gmail.com>
2017-05-15 12:26:09 -0700

    mongo set up complete

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 console.log("node-crud starting up!");
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
+app.use(bodyParser.urlencoded({extended: true}))
+
+MongoClient.connect('mongodb://sethgerou:spinspin@ds143241.mlab.com:43241/seth-jokes-node', (err, database) => {
+  if (err) console.log(err)
+  db = database
+  app.listen(3000, function() {
+    console.log('listening on 3000')
+  })
 })
 
 app.get('/', (req, res) => {
+  var cursor = db.collection('jokes').find().toArray(function(err, results){
+    console.log(results)
+  })
+
   res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/jokes', (req, res) => {
-  console.log('yay, submission received!')
+  db.collection('jokes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
